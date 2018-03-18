455ac51d4ea8f3f11057c49a33503ceafc8ddff7
it-spectre-ru <it.spectre.ru@gmail.com>
2018-03-18 18:26:28 +0500

    outputting dynamic content

--- burger/src/Person/Person.js
+++ burger/src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const person = () => {
-  return <p>A person!</p>
+  return <p>A person! and i am {Math.floor(Math.random() * 30)} years old</p>
 };
 
 export default person;
