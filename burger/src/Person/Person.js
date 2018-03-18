7491d59b675c641a9f7761cbdfa9d0605585e1a6
it-spectre-ru <it.spectre.ru@gmail.com>
2018-03-18 19:13:11 +0500

    the children property

--- burger/src/Person/Person.js
+++ burger/src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const person = (props) => {
-  return <p>A person! and i am {props.name} and {props.age} years old</p>
+  return (
+      <div>
+        <p>A person! and i am {props.name} and {props.age} years old</p>
+        <p>{props.children}</p>
+      </div>
+  );
 };
 
 export default person;
