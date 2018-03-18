500f2f0afb11084436dc6eb0dac8b66a40c94bdb
it-spectre-ru <it.spectre.ru@gmail.com>
2018-03-18 14:18:17 +0500

    creating a functional component

--- burger/src/App.js
+++ burger/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
+
 
 class App extends Component {
   render() {
@@ -7,6 +9,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, react app</h1>
         <p>this is really working</p>
+        <Person/>
       </div>
     );
     /*return React.createElement('div', {className: 'App'}, React.createElement('h1', '', 'now it works'));*/
