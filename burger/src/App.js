9305d749670dd2d7fe39f4f025a3d204c6c10f08
it-spectre-ru <it.spectre.ru@gmail.com>
2018-03-25 21:08:32 +0500

    enabling & using css modules

--- burger/src/App.js
+++ burger/src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import './App.css';
-import Radium, {StyleRoot} from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -53,10 +52,6 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black',
-      },
     };
 
     let persons = null;
@@ -77,10 +72,6 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black',
-      };
     }
 
     let classes = [];
@@ -92,7 +83,6 @@ class App extends Component {
     }
 
     return (
-        <StyleRoot>
           <div className="App">
             <h1>Hi, I'm a React App</h1>
             <p className={classes.join(' ')}>This is really working!</p>
@@ -102,11 +92,10 @@ class App extends Component {
             </button>
             {persons}
           </div>
-        </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'},
     // React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default Radium(App);
+export default App;
