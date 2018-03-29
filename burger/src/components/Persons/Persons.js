9105b49ef999a3557cd44fa241a25d635a5c96e8
it-spectre-ru <it.spectre.ru@gmail.com>
2018-03-29 23:29:30 +0500

    component updating lifecycle in action

--- burger/src/components/Persons/Persons.js
+++ burger/src/components/Persons/Persons.js
@@ -12,8 +12,25 @@ class Persons extends Component {
     console.log('[Persons.js] inside componentWillMount()');
   }
 
-  componentDidMount() {
+  componentDidMount () {
     console.log('[Persons.js] inside componentDidMount()');
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log('[UPDATE Persons.js] Inside componentWillReceiveProps', nextProps);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[UPDATE Persons.js] Inside shouldComponentUpdate', nextProps, nextState);
+    return nextProps.persons !== this.props.persons;
+  }
+
+  componentWillUpdate(nextProps, nextState){
+    console.log('[UPDATE Persons.js] Inside componentWillUpdate', nextProps, nextState)
+  }
+
+  componentDidUpdate () {
+    console.log('[UPDATE Persons.js] Inside componentDidUpdate')
   }
 
   render() {
