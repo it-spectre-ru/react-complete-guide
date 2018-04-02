9fb22d48949d64dea3bb7f0c64f3d39cfa251261
it-spectre-ru <it.spectre.ru@gmail.com>
2018-04-02 22:25:07 +0500

    outputting burger ingredients dynamically

--- burger-app/src/components/Burger/BurgerIngridient/BurgerIngredient.js
+++ burger-app/src/components/Burger/BurgerIngridient/BurgerIngredient.js
@@ -28,7 +28,7 @@ class BurgerIngredient extends Component {
       case ('bacon'):
         ingredient = <div className={classes.Bacon}></div>;
         break;
-      case ('salsd'):
+      case ('salad'):
         ingredient = <div className={classes.Salad}></div>;
         break;
       default:
