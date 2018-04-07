db31062c788cd88fb7b4899332abd3724df10950
it-spectre-ru <it.spectre.ru@gmail.com>
2018-04-07 21:38:26 +0500

    adding the price to the order summary

--- burger-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -19,6 +19,7 @@ const orderSummary = (props) => {
         <ul>
           {ingredienstSummary}
         </ul>
+        <strong><p>Total price: {props.price.toFixed(2)}</p></strong>
         <p>Continue to checkout?</p>
         <Button btnType="Danger" clicked={props.purchaseCanceled}>Cancel</Button>
         <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
