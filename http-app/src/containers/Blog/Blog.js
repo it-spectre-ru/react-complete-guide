f96cc6b1aefc10eb37fa4ab2529f9350c528c22c
it-spectre-ru <it.spectre.ru@gmail.com>
2018-04-10 23:02:15 +0500

    rendering fetched data to the screen

--- http-app/src/containers/Blog/Blog.js
+++ http-app/src/containers/Blog/Blog.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 
 import Post from '../../components/Post/Post';
@@ -8,30 +8,37 @@ import './Blog.css';
 
 class Blog extends Component {
 
-  componentDidMount () {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(response => {
-          console.log(response);
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+      this.setState({posts: response.data});
+      // console.log(response);
     });
   }
 
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  render() {
+
+    const posts = this.state.posts.map(post => {
+      return <Post key={post.id} title={post.title}/>;
+    });
+
+    return (
+        <div>
+          <section className="Posts">
+            {posts}
+          </section>
+          <section>
+            <FullPost/>
+          </section>
+          <section>
+            <NewPost/>
+          </section>
+        </div>
+    );
+  }
 }
 
 export default Blog;
