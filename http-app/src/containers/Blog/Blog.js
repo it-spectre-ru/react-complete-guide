f104f207271a76aa07052dd6c31b6ac3b5f234ba
it-spectre-ru <it.spectre.ru@gmail.com>
2018-05-05 22:18:48 +0500

    understanding nested routes

--- http-app/src/containers/Blog/Blog.js
+++ http-app/src/containers/Blog/Blog.js
@@ -1,43 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 // import axios from 'axios';
-import {Route, NavLink, Switch} from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
-
-  render() {
-
+  render () {
     return (
         <div className="Blog">
           <header>
             <nav>
               <ul>
                 <li><NavLink
-                    to="/"
+                    to="/posts/"
                     exact
                     activeClassName="my-active"
                     activeStyle={{
                       color: '#fa923f',
                       textDecoration: 'underline'
-                    }}>Home</NavLink></li>
+                    }}>Posts</NavLink></li>
                 <li><NavLink to={{
-                    pathname: '/new-post',
-                    hash: '#submit',
-                    search: '?quick-submit=true'
-                }}>New post</NavLink></li>
+                  pathname: '/new-post',
+                  hash: '#submit',
+                  search: '?quick-submit=true'
+                }}>New Post</NavLink></li>
               </ul>
             </nav>
           </header>
-         {/* <Route path="/" exact render={() => <h1>Home</h1> } />
-          <Route path="/" render={() => <h1>Home 2</h1> } />*/}
-          <Route path="/" exact component={Posts}/>
+          {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
           <Switch>
-            <Route path="/new-post" component={NewPost}/>
-            <Route path="/:id" exact component={FullPost}/>
+            <Route path="/new-post" component={NewPost} />
+            <Route path="/posts" component={Posts} />
           </Switch>
         </div>
     );
