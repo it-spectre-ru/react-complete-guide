01b91f9d3b11284c0d358ea06e94ae6397cbd992
it-spectre-ru <it.spectre.ru@gmail.com>
2018-05-15 08:48:18 +0500

    handling user input

--- burger-app/src/components/UI/Input/Input.js
+++ burger-app/src/components/UI/Input/Input.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import classes from './Input.css';
 
-
 const input = (props) => {
   let inputElement = null;
 
@@ -11,20 +10,23 @@ const input = (props) => {
       inputElement = <input
           className={classes.InputElement}
           {...props.elementConfig}
-          value={props.value}/>;
+          value={props.value}
+          onChange={props.changed}/>;
       break;
     case ('textarea'):
       inputElement = <textarea
           className={classes.InputElement}
           {...props.elementConfig}
-          value={props.value}/>;
+          value={props.value}
+          onChange={props.changed}/>;
       break;
     case ('select'):
       inputElement = (
           <select
               className={classes.InputElement}
               {...props.elementConfig}
-              value={props.value}>
+              value={props.value}
+              onChange={props.changed}>
             {props.elementConfig.option.map(option => (
                 <option key={option.value} value={option.value}>
                   {option.displayValue}
@@ -38,7 +40,8 @@ const input = (props) => {
       inputElement = <input
           className={classes.InputElement}
           {...props.elementConfig}
-          value={props.value}/>;
+          value={props.value}
+          onChange={props.changed}/>;
   }
 
   return (
