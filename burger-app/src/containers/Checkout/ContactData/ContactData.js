46a0adedea97e03f9ea08ca216c2dcc4fc8572fa
it-spectre-ru <it.spectre.ru@gmail.com>
2018-07-25 23:20:50 +0500

    handling overall form validity

--- burger-app/src/containers/Checkout/ContactData/ContactData.js
+++ burger-app/src/containers/Checkout/ContactData/ContactData.js
@@ -90,9 +90,11 @@ class ContactData extends Component {
             }
           ]
         },
-        value: ''
+        value: '',
+        valid: true
       },
     },
+    formIsValid: false,
     loading: false
   }
 
@@ -149,8 +151,12 @@ class ContactData extends Component {
     updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
     updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
-    console.log(updatedFormElement);
-    this.setState({orderForm: updatedOrderForm});
+
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+    this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
   }
 
   render () {
@@ -175,7 +181,7 @@ class ContactData extends Component {
                   changed={(event) => this.inputChangedHandler(event, formElement.id)}
               />
           ))}
-          <Button btnType="Success">Order</Button>
+          <Button btnType="Success" disabled={!this.state.formIsValid}>Order</Button>
         </form>
     );
     if (this.state.loading) {
