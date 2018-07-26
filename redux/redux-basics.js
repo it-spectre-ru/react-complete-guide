ddd2ae93521b6a81873ec932caf857ab85a93aa0
it-spectre-ru <it.spectre.ru@gmail.com>
2018-07-26 23:30:26 +0500

    dispatching actions

--- redux/redux-basics.js
+++ redux/redux-basics.js
@@ -2,12 +2,24 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 const initialState = {
-  counter: 0
-}
-
+  counter: 0,
+};
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
+  if (action.type === 'INC_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === 'ADD_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    };
+  }
   return state;
 };
 
@@ -15,8 +27,9 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
-
-
 // Dispatching Action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
 
 // Subscription
