4f34ff7c601d732858720c75bd1d79e11b76a45f
it-spectre-ru <it.spectre.ru@gmail.com>
2018-08-07 21:26:55 +0500

    action creators & async code

--- redux/src/containers/Counter/Counter.js
+++ redux/src/containers/Counter/Counter.js
@@ -1,30 +1,25 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import * as actionCreators from '../../store/actions/actions';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
-import * as actionTypes from '../../store/actions';
+import * as actionTypes from '../../store/actions/actions';
 
 class Counter extends Component {
   render() {
     return (
         <div>
           <CounterOutput value={this.props.ctr}/>
-          <CounterControl label="Increment"
-                          clicked={this.props.onIncrementCounter}/>
-          <CounterControl label="Decrement"
-                          clicked={this.props.onDecrementCounter}/>
+          <CounterControl label="Increment" clicked={this.props.onIncrementCounter}/>
+          <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}/>
           <CounterControl label="Add 10" clicked={this.props.onAddCounter}/>
-          <CounterControl label="Subtract 15"
-                          clicked={this.props.onSubtractCounter}/>
+          <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}/>
           <hr/>
-          <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store
-            Result
-          </button>
+          <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
           <ul>
             {this.props.storedResults.map(strResult => (
-                <li key={strResult.id} onClick={() => this.props.onDeleteResult(
-                    strResult.id)}>{strResult.value}</li>
+                <li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
             ))}
           </ul>
         </div>
@@ -42,14 +37,12 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
-    onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
-    onAddCounter: () => dispatch({type: actionTypes.ADD, val: 10}),
-    onSubtractCounter: () => dispatch({type: actionTypes.SUBTRACT, val: 15}),
-    onStoreResult: (result) => dispatch(
-        {type: actionTypes.STORE_RESULT, result: result}),
-    onDeleteResult: (id) => dispatch(
-        {type: actionTypes.DELETE_RESULT, resultElId: id}),
+    onIncrementCounter: () => dispatch(actionCreators.increment()),
+    onDecrementCounter: () => dispatch(actionCreators.decrement()),
+    onAddCounter: () => dispatch(actionCreators.add(10)),
+    onSubtractCounter: () => dispatch(actionCreators.subtract(15)),
+    onStoreResult: (result) => dispatch(actionCreators.storeResult(result)),
+    onDeleteResult: (id) => dispatch(actionCreators.deleteResult(id)),
   };
 };
 
