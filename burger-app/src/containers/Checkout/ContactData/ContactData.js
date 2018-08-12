532f642f488e7c557de157c3237ce58e27f71e72
it-spectre-ru <it.spectre.ru@gmail.com>
2018-08-12 19:20:54 +0500

    combining reducers

--- burger-app/src/containers/Checkout/ContactData/ContactData.js
+++ burger-app/src/containers/Checkout/ContactData/ContactData.js
@@ -87,7 +87,7 @@ class ContactData extends Component {
             {value: 'cheapest',displayValue: 'Cheapest'}
           ]
         },
-        value: '', /*in ui we will see fastest/fastest but if never switch options behind empty strings*/
+        value: 'fastest', /*in ui we will see fastest/fastest but if never switch options behind empty strings*/
         validation: {},
         valid: true
       },
@@ -191,9 +191,9 @@ class ContactData extends Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.ingredients,
-    price: state.totalPrice,
-    loading: state.loading
+    ings: state.burgerBuilder.ingredients,
+    price: state.burgerBuilder.totalPrice,
+    loading: state.order.loading
   }
 };
 
