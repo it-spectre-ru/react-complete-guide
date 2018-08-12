1603f25fcac2ef46dc3eda7ad6175528dd9b0d84
it-spectre-ru <it.spectre.ru@gmail.com>
2018-08-12 18:42:01 +0500

    redirect to improve UX

--- burger-app/src/containers/Checkout/Checkout.js
+++ burger-app/src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
@@ -9,31 +9,36 @@ class Checkout extends Component {
 
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
-  }
+  };
 
   checkoutContinuedHandler = () => {
     this.props.history.replace('/checkout/contact-data');
-  }
+  };
 
   render() {
-    return (
-        <div>
-          <CheckoutSummary
-              ingredients={this.props.ings}
-              checkoutCancelled={this.checkoutCancelledHandler}
-              checkoutContinued={this.checkoutContinuedHandler} />
-          <Route
-              path={this.props.match.path + '/contact-data'}
-              component={ContactData} />
-        </div>
-    );
+    let summary = <Redirect to="/"/>;
+    if (this.props.ing) {
+      summary = (
+          <div>
+            <CheckoutSummary
+                ingredients={this.props.ings}
+                checkoutCancelled={this.checkoutCancelledHandler}
+                checkoutContinued={this.checkoutContinuedHandler}/>
+            <Route
+                path={this.props.match.path + '/contact-data'}
+                component={ContactData}/>
+          </div>
+      );
+    }
+
+    return summary;
   }
 }
 
 const mapStateToProps = state => {
   return {
-    ings: state.ingredients
-  }
-}
+    ings: state.ingredients,
+  };
+};
 
 export default connect(mapStateToProps)(Checkout);
